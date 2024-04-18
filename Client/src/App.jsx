2d15b147cbe96a9aa5c8f9f886822cf2506cf754
areal <arealgizaw@gmail.com>
2024-04-18 15:39:50 +0300

    install packages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <h1 className="bg-sky-700 px-4 py-2  text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-      Hello world!
-    </h1>
-  );
+  return <></>;
 }
 
 export default App;
