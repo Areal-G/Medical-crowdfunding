b4bf63ad7c404ab20d152b04df3ced8a57d4c4cd
areal <arealgizaw@gmail.com>
2024-04-23 12:36:02 +0300

    client contact us page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,21 +1,25 @@
 import About from "./components/About";
-import Campaign from "./components/Campaign";
+import CampaignComp from "./components/CampaignComp";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Nav from "./components/Nav";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
+import ContactUs from "./components/ContactUs";
 
 function App() {
   return (
     <>
       <Nav />
-      <SignUp />
-      <SignIn />
-      {/* <Main />
-      <Campaign />
+
+      {/* <SignUp />
+      <SignIn /> */}
+
+      <Main />
+      <CampaignComp />
       <About />
-      <Footer /> */}
+      <ContactUs />
+      <Footer />
     </>
   );
 }
--- Client/src/components/CampaignCard.jsx
+++ Client/src/components/CampaignCard.jsx
@@ -2,7 +2,7 @@ import e from "../assets/img/e.jpeg";
 const CampaignCard = () => {
   return (
     <div className="card mx-auto mt-5 max-w-96 rounded-xl shadow-2xl">
-      <div className="max-w-sm bg-white dark:bg-gray-800">
+      <div className="max-w-sm rounded-xl bg-white dark:bg-gray-800">
         <div className="  mx-auto mt-8 h-60 w-80 overflow-hidden rounded-lg shadow-xl">
           <a href="#">
             <img className=" " src={e} alt="" />
@@ -11,7 +11,7 @@ const CampaignCard = () => {
 
         <div className="rounded-xl p-5">
           <a href="#">
-            <h5 className="text-primary-600 mb-2 text-2xl font-bold tracking-tight dark:text-white">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-primary-600 dark:text-white">
               Erdata to the website
             </h5>
           </a>
@@ -19,27 +19,10 @@ const CampaignCard = () => {
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio
             consequuntur dicta, molestiae soluta ullam sunt cumque,
           </p>
-          <a
-            href="#"
-            className="bg-primary-600 inline-flex items-center rounded-lg px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
+          {/* progress bar */}
+          <div className="mb-5 mt-5 h-2.5 w-[90%] rounded-full bg-gray-200 dark:bg-gray-700">
+            <div className="h-2.5 w-[45%] rounded-full bg-primary-600"></div>
+          </div>
         </div>
       </div>
     </div>
